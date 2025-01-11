839273918b644b4674b7f797c7fce701f96c3454
Karina Myronova <karinamyronova@MacBookAir.fritz.box>
2025-01-11 13:50:49 +0100

    hw4

--- js/task-1.js
+++ js/task-1.js
@@ -1,26 +1,8 @@
-function slugify(title) {
-  return title
-    .toLowerCase()               
-    .trim()                       
-    .replace(/\s+/g, '-')         
-    .replace(/[^a-z0-9\-]/g, '')   
-    .replace(/--+/g, '-')         
-    .replace(/^-+/, '')          
-    .replace(/-+$/, '');           
-}
-
-// Тестування функції
-console.log(slugify("Arrays for beginners")); // "arrays-for-beginners"
-console.log(slugify("English for developer")); // "english-for-developer"
-console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
-
 
 function isEnoughCapacity(products, containerSize) {
-  // Обчислюємо загальну кількість товарів
+  
   const totalProducts = Object.values(products).reduce((sum, quantity) => sum + quantity, 0);
 
-  // Перевіряємо, чи поміщаються товари в контейнер
   return totalProducts <= containerSize;
 }
 
